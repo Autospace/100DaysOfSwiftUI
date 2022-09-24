2a344b86a2587bd9ffd8a4a741f561c6198df3a2
Alex Mostovnikov <mostovnikov.alex@gmail.com>
2022-09-24 16:05:33 +0200

    WeSplit initial commit

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Alex Mostovnikov on 24/9/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
