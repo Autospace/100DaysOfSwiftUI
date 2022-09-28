fa434f886fe021fa62e5c981c835af149e98b5e7
Alex Mostovnikov <mostovnikov.alex@gmail.com>
2022-09-28 19:43:06 +0200

    Day 19. Create LengthConverter app

---
+++ LengthConverter/LengthConverter/LengthConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LengthConverterApp.swift
+//  LengthConverter
+//
+//  Created by Alex Mostovnikov on 28/9/22.
+//
+
+import SwiftUI
+
+@main
+struct LengthConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
