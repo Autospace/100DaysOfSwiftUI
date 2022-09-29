dd82bae4fb95044331de0e11d2d37c8ac69e01f8
Alex Mostovnikov <mostovnikov.alex@gmail.com>
2022-09-29 22:50:08 +0200

    Day 20. Create GuessTheFlag app

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Alex Mostovnikov on 29/9/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
