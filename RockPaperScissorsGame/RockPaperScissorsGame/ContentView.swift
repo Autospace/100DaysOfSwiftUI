06da396968ba6233280c90325364d156a6bfbe2f
Alex Mostovnikov <mostovnikov.alex@gmail.com>
2022-10-20 20:14:13 +0200

    Day 25. Update title for RockPaperScissorsGame app

--- RockPaperScissorsGame/RockPaperScissorsGame/ContentView.swift
+++ RockPaperScissorsGame/RockPaperScissorsGame/ContentView.swift
@@ -13,7 +13,8 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Rock, Paper, Scissors game")
+                .padding()
         }
         .padding()
     }
