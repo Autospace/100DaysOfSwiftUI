2425eeed3e6d8ca45bd51f25445e586b70091cd2
Alex Mostovnikov <mostovnikov.alex@gmail.com>
2022-10-20 20:12:09 +0200

    Day 25. Create RockPaperScissorsGame app

---
+++ RockPaperScissorsGame/RockPaperScissorsGame/RockPaperScissorsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsGameApp.swift
+//  RockPaperScissorsGame
+//
+//  Created by Alex Mostovnikov on 20/10/22.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
