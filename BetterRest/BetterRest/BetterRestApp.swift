ec6f9a2d1dd965c1089bbcb0f1c9be71d2649951
Aliaksei Mastounikau <amastoun@its.jnj.com>
2022-11-03 19:49:14 +0100

    Day 26. Create better rest application stup

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Aliaksei Mastounikau on 3.11.22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
